e88dfe7f9f952d002d342d66bcd815b1e0fac116
Milos Milijasevic <milosrm20005@gmail.com>
2022-06-08 12:18:13 +0200

    updated

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,25 +2,58 @@ import React from "react";
 
 const Quiz = (props) =>{
 
-    console.log(props.quiz)
+    const [playing, setPlaying] = React.useState(true);
+
+    const [number, setNumber] = React.useState(0);
+
+    const changeNumber = () =>{
+
+        if(number === 9){
+            setPlaying(false);
+        }
+
+        setNumber(prevNumber => prevNumber + 1);
+        
+    };
+
+    const playAgain = () =>{
+        setPlaying(true);
+        setNumber(0);
+    };
+
 
     return(
 
         <div className="quiz-overlay">
+        {playing === true ?  
+        <>
 
-            <p className="question-number">Question 1/10</p>
-
-            <p className="question-text">{props.quiz[0].question}</p>
-
+            <p className="question-number">Question {number + 1}/10</p>
+            
+            <p className="question-text">{props.quiz[number].question}</p>
+            
             <div className="inline">
-                <p className="answer">A: <span>{props.quiz[0].options[0].answer}</span></p>
-                <p className="answer">B: <span>{props.quiz[0].options[1].answer}</span></p>
+            <p className="answer" onClick={changeNumber}>A: <span>{props.quiz[number].options[0].answer}</span></p>
+            <p className="answer" onClick={changeNumber}>B: <span>{props.quiz[number].options[1].answer}</span></p>
+            </div>
+            
+            <div className="inline">
+                <p className="answer" onClick={changeNumber}>C: <span>{props.quiz[number].options[2].answer}</span></p>
+                <p className="answer" onClick={changeNumber}>D: <span>{props.quiz[number].options[3].answer}</span></p>
             </div>
 
-            <div className="inline">
-                <p className="answer">C: <span>{props.quiz[0].options[2].answer}</span></p>
-                <p className="answer">D: <span>{props.quiz[0].options[3].answer}</span></p>
+        </> : 
+        
+        <>
+        
+            <div className="score">
+                <p className="score-info">You score is 10/10</p>
+                <button className="button-play-again" onClick={playAgain}>Play Again</button>
             </div>
+
+        </>
+        
+        }
 
         </div>
 
