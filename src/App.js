ea9defc28fb9b27b3947479a498b36d39c75d733
Milos Milijasevic <milosrm20005@gmail.com>
2022-06-09 10:13:06 +0200

    app.js updated

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ const App = () =>{
         {
             question: "What is Earth's largest continent?",
             options: [
-                {answer: "Asia", isCorrect: true},
                 {answer: "Europe", isCorrect: false},
                 {answer: "Africa", isCorrect: false},
+                {answer: "Asia", isCorrect: true},
                 {answer: "Antarctica", isCorrect: false}
             ]
         },
@@ -17,8 +17,8 @@ const App = () =>{
         {
             question: "What is the tallest mountain in the world?",
             options: [
-                {answer: "Mount Everest", isCorrect: true},
                 {answer: "Mount Kilamanjaro", isCorrect: false},
+                {answer: "Mount Everest", isCorrect: true},
                 {answer: "Aconcagua", isCorrect: false},
                 {answer: "Qogir", isCorrect: false}
             ]
@@ -27,10 +27,10 @@ const App = () =>{
         {
             question: "What is the flattest continent?",
             options: [
-                {answer: "Australia", isCorrect: true},
                 {answer: "South America", isCorrect: false},
                 {answer: "Antarctica", isCorrect: false},
-                {answer: "Africa", isCorrect: false}
+                {answer: "Africa", isCorrect: false},
+                {answer: "Australia", isCorrect: true}
             ]
         },
 
@@ -47,9 +47,9 @@ const App = () =>{
         {
             question: "What is the highest waterfall in Europe?",
             options: [
+                {answer: "Kjelfossen", isCorrect: false},
                 {answer: "Krimml", isCorrect: true},
                 {answer: "Rhine", isCorrect: false},
-                {answer: "Kjelfossen", isCorrect: false},
                 {answer: "Triberg", isCorrect: false}
             ]
         },
@@ -57,10 +57,10 @@ const App = () =>{
         {
             question: "Montevideo is the capital of what South American country?",
             options: [
-                {answer: "Uruguay", isCorrect: true},
                 {answer: "Peru", isCorrect: false},
                 {answer: "Paraguay", isCorrect: false},
-                {answer: "Suriname", isCorrect: false}
+                {answer: "Suriname", isCorrect: false},
+                {answer: "Uruguay", isCorrect: true}
             ]
         },
 
@@ -77,18 +77,19 @@ const App = () =>{
         {
             question: "What is the only major city located on two continents?",
             options: [
-                {answer: "Instanbul", isCorrect: true},
                 {answer: "Rome", isCorrect: false},
                 {answer: "New Delhi", isCorrect: false},
-                {answer: "London", isCorrect: false}
+                {answer: "London", isCorrect: false},
+                {answer: "Instanbul", isCorrect: true}
+
             ]
         },
 
         {
             question: "What is the largest country in the world in terms of land area?",
             options: [
-                {answer: "Russia", isCorrect: true},
                 {answer: "Canada", isCorrect: false},
+                {answer: "Russia", isCorrect: true},
                 {answer: "United States", isCorrect: false},
                 {answer: "China", isCorrect: false}
             ]
@@ -97,9 +98,9 @@ const App = () =>{
         {
             question: "What country has the most natural lakes?",
             options: [
-                {answer: "Canada", isCorrect: true},
                 {answer: "Australia", isCorrect: false},
                 {answer: "India", isCorrect: false},
+                {answer: "Canada", isCorrect: true},
                 {answer: "Unated States", isCorrect: false}
             ]
         }
